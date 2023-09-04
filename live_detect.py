cebf9ebf2be293a1effeaa1dd64d51dedaad76ee
Monique Caldeira <86532831+MoniqueCaldeira@users.noreply.github.com>
2023-09-04 14:54:15 -0300

    Update live_detect.py

--- live_detect.py
+++ live_detect.py
@@ -2,7 +2,6 @@
 import cv2
 
 #Carregue o arquivo do classificador cascade
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml') 
 
 # Defina um objeto VideoCapture
 vid = cv2.VideoCapture(0)
@@ -13,14 +12,10 @@ while(True):
     ret, frame = vid.read()
 
     # Converta para escala de cinza
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     # Detecte os rostos
-    faces = face_cascade.detectMultiScale(gray, 1.1, 5)
 
     # Desenhe o retângulo ao redor de cada rosto
-    for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
         
     # Exiba o quadro resultante
     cv2.imshow('frame', frame)
